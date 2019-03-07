ad46fad765a64fdc314aa9af2756cb6442e4f349
Jesús Rivas Jiménez <jesus.rivas.jmnez@gmail.com>
2019-03-07 23:34:17 +0100

    @jesusrivasjmnez - Initial project

--- scripts/merge_branch.sh
+++ scripts/merge_branch.sh
@@ -7,16 +7,21 @@ echo "Target branch: $TRAVIS_BRANCH"
 
 if [ "$TRAVIS_PULL_REQUEST" != "$NOT_PR" ]; then
   echo "Merging PR into $TRAVIS_BRANCH"
- 
-  git fetch origin
-  git checkout -b $TRAVIS_PULL_REQUEST_BRANCH origin/$TRAVIS_PULL_REQUEST_BRANCH
+
+  echo $SSH_KEY > ~/.ssh/id_rsa.pub
+  ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
+  git remote add upstream git@github.com:barlingo-app/ci-cd-test.git
+
+  git fetch upstream
+  git checkout $TRAVIS_PULL_REQUEST_BRANCH
   git merge $TRAVIS_BRANCH
   
   git checkout $TRAVIS_BRANCH
-  git merge --no-ff TRAVIS_PULL_REQUEST_BRANCH
-  git push origin $TRAVIS_BRANCH
+  git merge --no-ff $TRAVIS_PULL_REQUEST_BRANCH
+  git push upstream $TRAVIS_BRANCH
 
-  git push --delete origin $TRAVIS_PULL_REQUEST_BRANCH
+  git push --delete upstream $TRAVIS_PULL_REQUEST_BRANCH
 fi
 
 exit 0
