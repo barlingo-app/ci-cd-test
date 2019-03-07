bdc44d8b02b92b949c4281d4139fc272a447abbe
Jesús Rivas Jiménez <jesus.rivas.jmnez@gmail.com>
2019-03-07 20:44:13 +0100

    @jesusrivasjmnez - Travis CI integration

---
+++ scripts/merge_branch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+if [[ "$TRAVIS_PULL_REQUEST" = "true" ]]; then
+  echo "Merging PR into $TRAVIS_BRANCH"
+ 
+  git fetch origin
+  git checkout -b $TRAVIS_PULL_REQUEST_BRANCH origin/$TRAVIS_PULL_REQUEST_BRANCH
+  git merge $TRAVIS_BRANCH
+  
+  git checkout $TRAVIS_BRANCH
+  git merge --no-ff TRAVIS_PULL_REQUEST_BRANCH
+  git push origin $TRAVIS_BRANCH
+fi
+
+exit 0
